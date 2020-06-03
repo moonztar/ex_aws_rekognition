296ebea85f284c86ce0a6484f861be27832b87f9
ga_c <ga_c@ymail.com>
2020-06-03 11:39:31 -0500

    mix file was updated and dependencies were added

--- mix.exs
+++ mix.exs
@@ -1,15 +1,37 @@
 defmodule ExAwsRekognition.MixProject do
   use Mix.Project
 
+  @version "1.0.0"
+  @service "rekognition"
+  @url "https://github.com/moonztar/ex_aws_#{@service}"
+  @name __MODULE__ |> Module.split() |> Enum.take(2) |> Enum.join(".")
+
   def project do
     [
       app: :ex_aws_rekognition,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: @name,
+      package: package(),
+      docs: [main: @name, source_ref: "v#{@version}", source_url: @url]
     ]
   end
+
+  defp package do
+    [
+      description: "#{@name} service package",
+      files: ["lib", "config", "mix.exs", "README*"],
+      maintainers: ["moonztar"],
+      licenses: ["MIT"],
+      links: %{github: @url}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
@@ -23,6 +45,10 @@ defmodule ExAwsRekognition.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:ex_aws, "~> 2.1"},
+      {:poison, "~> 3.0"},
     ]
   end
 end
